e83d691533b7bddf2233b4873a66abe872c97062
Rafaf Tahsin <rafaftahsin@gmail.com>
2021-06-17 11:28:15 +0600

    circles

--- src/org/tahsin/Circle.java
+++ src/org/tahsin/Circle.java
@@ -1,5 +1,7 @@
 package org.tahsin;
 
+import org.springframework.beans.factory.annotation.Required;
+
 public class Circle implements Shape {
 	
 	private Point center;
@@ -11,7 +13,7 @@ public class Circle implements Shape {
 	}
 
 
-
+	@Required
 	public void setCenter(Point center) {
 		this.center = center;
 	}

54735b7a9ed5d7c1b34555d99f9b6c1bf76c9b85
Rafaf Tahsin <rafaftahsin@gmail.com>
2021-06-23 13:27:36 +0600

    PointCut

--- src/org/tahsin/aspect/LoggingAspect.java
+++ src/org/tahsin/aspect/LoggingAspect.java
@@ -2,14 +2,24 @@ package org.tahsin.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 
 @Aspect 
 public class LoggingAspect {
 	
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void LoggingAdvice() {
 		System.out.println("Advice run. Get Method called");
 	}
+	
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second Advice Executed");
+	}
+	
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
 
 }

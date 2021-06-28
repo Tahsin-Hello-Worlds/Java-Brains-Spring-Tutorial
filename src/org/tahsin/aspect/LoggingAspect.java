a39c4d18aaca87b50df97f605d46bf3c78d6be15
Rafaf Tahsin <rafaftahsin@gmail.com>
2021-06-28 12:04:28 +0600

    added aspect definition in spring.xml

--- src/org/tahsin/aspect/LoggingAspect.java
+++ src/org/tahsin/aspect/LoggingAspect.java
@@ -15,32 +15,8 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect 
 public class LoggingAspect {
 	
-	@After("allGetters() && allCircleMethods()")
 	
-	// JoinPoint is the method that called this Advice 
-	public void LoggingAdvice(JoinPoint joinPoint) {
-		// Circle circle = (Circle) joinPoint.getTarget();
-		
-		// System.out.println(joinPoint.getTarget());
-	}
-	
-	@AfterReturning(pointcut="args(name)", returning="returnString")
-	public void stringArgumentMethods(String name, String returnString) {
-		System.out.println("A method that takes string arguments has been called. The value is " + name + "The output value is " + returnString);
-	}
-	
-	
-	@AfterReturning("allGetters()")
-	public void secondAdvice() {
-		System.out.println("Second Advice Executed");
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing="ex") 
-	public void exceptionAdvice(String name, RuntimeException ex){
-		System.out.println("An Exception has been thrown" + ex);
-	}
-	
-	@Around("@annotation(org.tahsin.aspect.Loggable)")
+	@Around("allGetters()")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
 		
 		Object returnValue = null;
@@ -61,8 +37,5 @@ public class LoggingAspect {
 	@Pointcut("execution(* get*())")
 	public void allGetters() {}
 	
-	
-	@Pointcut("within(org.tahsin.model.Circle)")
-	public void allCircleMethods() {}
 	
 }
